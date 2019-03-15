163963720fea1f9260a65295ad52962b39a35d5b
nfiscus <44373459+nfiscus@users.noreply.github.com>
2019-03-14 20:23:51 -0500

    update

--- scripts/myScript.js
+++ scripts/myScript.js
@@ -1,7 +1,3 @@
-window.addEventListener("load", pageFullyLoaded, false);
-
-function pageFullyLoaded(){
-    var userName = window.prompt('Welcome, guest. Please enter your name: ');
-    window.alert('Hello ' + userName + '!');
-    console.log('Hello ' + userName + '!');
-};
+var userName = window.prompt('Welcome, guest. Please enter your name: ');
+window.alert('Hello ' + userName + '!');
+console.log('Hello ' + userName + '!');
